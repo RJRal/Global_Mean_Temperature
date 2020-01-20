fe093426c81b2897221190f24c89d18965ef64dd
RJRal <51172390+RJRal@users.noreply.github.com>
2020-01-20 17:51:02 +0100

    Linear Regression for GISTEMP
    I apply the simplest machine learning methods: Linear Regression.

---
+++ GISTEMP_LinearRegresion.py
@@ -0,0 +1,48 @@
+
+
+# This is a script to apply the simplest form of machine learning: Linear Regression 
+
+# I will use the GISTEMP global mean temperature data 1880-2014
+
+import csv
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plot
+
+file_name="/home/rusul/Desktop/Upload/global-temp-annual_csv.csv"
+
+df = pd.read_csv(file_name) # This gives you a data frame so better use commands that are useful in data frames
+
+df.head() # From this we see that column 'Land and Ocean' is what I want (because these are mean values of temperature over both land and ocean
+
+X = df[['Land and Ocean']]
+
+y = pd.DataFrame(np.array(np.arange(len(X)))) 
+#x = df.iloc[1, :-1].values
+
+#y = df.iloc[:, 2].values
+
+xTrain, xTest, yTrain, yTest = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+linearRegressor = LinearRegression()
+
+linearRegressor.fit(xTrain, yTrain)
+
+yPrediction = linearRegressor.predict(xTest)
+
+print(xTrain.shape)
+print(yTrain.shape)
+print(xTest.shape)
+print(yTest.shape)
+print(yPrediction.shape)
+
+
+plot.scatter(xTrain, yTrain, color = 'teal')
+plot.plot(xTrain, linearRegressor.predict(xTrain), color = 'firebrick')
+plot.title('Temperature vs Time (Training set)')
+plot.xlabel('Time')
+plot.ylabel('Temperature')
+plot.show()
+
